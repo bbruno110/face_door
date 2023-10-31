34ccdfea06a4d006f63cdfc722f218220fcd8b2d
bbruno110 <bruno.os230@gmail.com>
2023-10-31 16:22:37 -0300

    criacao do registro de usuarios, upload de imagem

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 export const api = axios.create({
     //baseURL: 'http://10.0.0.133:8081'
-    //baseURL: 'https://face-door-back.onrender.com'
-    baseURL: 'http://192.168.1.22:8081'
+    baseURL: 'https://face-door-back.onrender.com'
+    //baseURL: 'http://192.168.1.22:8081'
 });
