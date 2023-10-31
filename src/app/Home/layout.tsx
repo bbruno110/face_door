6241926fdc96ea51295f0da65038427bcba24cf9
bbruno110 <bruno.os230@gmail.com>
2023-10-31 17:29:04 -0300

    criacao do registro de usuarios, upload de imagem

--- src/app/Home/layout.tsx
+++ src/app/Home/layout.tsx
@@ -8,10 +8,7 @@ interface Props {
 
 export default function InitialLayout({children}: {children: React.ReactNode}) {
   return (
-    <main  className="flex">
-      <div className=' z-10'>
-        <Sidebar/>
-      </div>
+    <main>
       <ToastContainer
           position="top-right"
           autoClose={1000}
@@ -24,10 +21,16 @@ export default function InitialLayout({children}: {children: React.ReactNode}) {
           pauseOnHover
           theme="light"
       />
-            <div className="w-full overflow-y-auto h-auto z-0 mx-auto lg:m-1 m-3
-         bg-[#071422]">
-              {children}
-            </div>
+      <div className="flex">
+        <div className=' z-10'>
+          <Sidebar/>
+        </div>
+        
+        <div className="w-full overflow-y-auto h-auto z-0 mx-auto lg:m-1 m-3
+    bg-[#071422]">
+          {children}
+        </div>
+      </div>
     </main>
   )
 }
