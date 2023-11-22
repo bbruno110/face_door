b27c4444a4698feaad28a9742d651908c4223777
bbruno110 <bruno.os230@gmail.com>
2023-11-22 17:26:27 -0300

    configuracao botao excluir

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = "http://192.168.1.22:8081"
+export const url = "https://face-door-back.onrender.com"
 
 export const api = axios.create({
     //baseURL: 'http://10.0.0.133:8081'
